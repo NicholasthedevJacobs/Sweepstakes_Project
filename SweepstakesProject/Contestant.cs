5a77311def42ff53d75a1fd2576bcf0427b79c6b
NicholasthedevJacobs <jjacobs.nicholas@gmail.com>
2020-08-30 16:15:39 -0500

    Created the Contestant class, and added member variables.

---
+++ SweepstakesProject/Contestant.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SweepstakesProject
+{
+    class Contestant
+    {
+        //member variables
+        string firstName;
+        string lastName;
+        string emailAddress;
+        int registrationNumber;
+
+        //constructor
+
+        //member methods
+
+
+    }
+}
