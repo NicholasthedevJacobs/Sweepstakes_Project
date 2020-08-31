b87b68d9b496fdbc959148e1c58d3bd914e5e83f
NicholasthedevJacobs <jjacobs.nicholas@gmail.com>
2020-08-31 11:05:26 -0500

    Created an interface for the UI, and added ways to get various info for the user.

---
+++ SweepstakesProject/IUserInterface.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SweepstakesProject
+{
+    public interface IUserInterface
+    {
+        void FirstName();
+
+        void LastName();
+
+        void Email();
+
+        void RegistrationNumber();
+       
+    }
+}
