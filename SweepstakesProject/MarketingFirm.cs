ae9f5a84f7baec067c2a6f36862482ba369c3ed6
NicholasthedevJacobs <jjacobs.nicholas@gmail.com>
2020-09-01 08:44:37 -0500

    Filled the logic for the CreateSweepstakes method in the MarketingFirm class.

--- SweepstakesProject/MarketingFirm.cs
+++ SweepstakesProject/MarketingFirm.cs
@@ -12,7 +12,18 @@ namespace SweepstakesProject
         private ISweepStakesManager _manager;
 
         //constructor
+        public MarketingFirm(ISweepStakesManager manager)
+        {
+            this._manager = manager;
+        }
 
         //member methods
+        public void CreateSweepstakes()
+        {
+            Console.WriteLine("What would you like to name the Sweepstakes?");
+            string sweepstakesName = Console.ReadLine();
+            Sweepstakes sweepstakes = new Sweepstakes(sweepstakesName);
+            _manager.InsertSweepstakes(sweepstakes);           
+        }
     }
 }
